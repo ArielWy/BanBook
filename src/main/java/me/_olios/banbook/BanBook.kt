228376c7681b649e313d2621e2a9789b06bd9ee2
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-02-22 22:37:22 +0200

    initial commit - convert to kotlin

---
+++ src/main/java/me/_olios/banbook/BanBook.kt
@@ -0,0 +1,13 @@
+package me._olios.banbook
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BanBook : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
